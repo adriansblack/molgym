ea62b10759a20a248dbe433af566e425e1dddcda
Gregor Simm <gncsimm@gmail.com>
2021-12-08 16:38:35 +0000

    run YAPF

--- molgym/tools/__init__.py
+++ molgym/tools/__init__.py
@@ -1,14 +1,14 @@
 from .arg_parser import build_default_arg_parser
 from .checkpoint import CheckpointHandler, CheckpointIO, CheckpointState
 from .torch_tools import (to_one_hot, masked_softmax, to_numpy, set_seeds, init_device, TensorDict, count_parameters,
-                          set_default_dtype, concat_tensor_dicts, stack_tensor_dicts, apply_to_dict,
-                          dict_to_device, detach_tensor_dict)
+                          set_default_dtype, concat_tensor_dicts, stack_tensor_dicts, apply_to_dict, dict_to_device,
+                          detach_tensor_dict)
 from .train import train
 from .utils import parse_zs, setup_logger, get_tag, get_optimizer, ProgressLogger, random_train_valid_split
 
 __all__ = [
     'TensorDict', 'to_numpy', 'to_one_hot', 'masked_softmax', 'build_default_arg_parser', 'set_seeds', 'init_device',
     'parse_zs', 'setup_logger', 'get_tag', 'count_parameters', 'set_default_dtype', 'concat_tensor_dicts',
-    'stack_tensor_dicts', 'apply_to_dict', 'dict_to_device', 'get_optimizer', 'ProgressLogger',
-    'CheckpointHandler', 'CheckpointIO', 'CheckpointState', 'random_train_valid_split', 'train', 'detach_tensor_dict'
+    'stack_tensor_dicts', 'apply_to_dict', 'dict_to_device', 'get_optimizer', 'ProgressLogger', 'CheckpointHandler',
+    'CheckpointIO', 'CheckpointState', 'random_train_valid_split', 'train', 'detach_tensor_dict'
 ]
